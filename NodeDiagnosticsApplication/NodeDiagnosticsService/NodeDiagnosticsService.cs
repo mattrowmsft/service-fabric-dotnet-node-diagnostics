e4a30e87452f397b886cd702fd8d17ddf73526c8
Matt Trower <mattrow@ntdev.microsoft.com>
2017-04-11 13:44:27 -0700

    Create initial implementation of the NodeDiagnosticsApplicaiton using EventFlow.

---
+++ NodeDiagnosticsApplication/NodeDiagnosticsService/NodeDiagnosticsService.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NodeDiagnosticsService
+{
+    using System.Fabric;
+    using Microsoft.ServiceFabric.Services.Runtime;
+
+    /// <summary>
+    /// An instance of this class is created for each service instance by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class NodeDiagnosticsService : StatelessService
+    {
+        public NodeDiagnosticsService(StatelessServiceContext context)
+            : base(context)
+        { }
+    }
+}
